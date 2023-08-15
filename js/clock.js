a9767c7908fe05a76aee46c846210dca555098e3
novellus1013 <novellus1013@gmail.com>
2023-08-15 10:37:37 +0900

    clock.js

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector(".clock");
+
+function handleTime() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+}
+
+setInterval(handleTime, 1000);
