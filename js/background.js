380be24e174d3bf72f4b69b753436ac69e32447c
novellus1013 <novellus1013@gmail.com>
2023-08-20 00:14:19 +0900

    이미지 경로 수정

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 const images = [
-  "/assets/back.gif",
-  "/assets/1.jpg",
-  "/assets/2.jpg",
-  "/assets/3.jpg",
-  "/assets/4.jpg",
-  "/assets/5.jpg",
-  "/assets/6.jpg",
+  "./assets/back.gif",
+  "./assets/1.jpg",
+  "./assets/2.jpg",
+  "./assets/3.jpg",
+  "./assets/4.jpg",
+  "./assets/5.jpg",
+  "./assets/6.jpg",
 ];
 
 const body = document.querySelector("body");
