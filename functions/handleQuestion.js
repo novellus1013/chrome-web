ad906fe43ae023f34514c4d4232eba4ccab6746f
novellus1013 <novellus1013@gmail.com>
2023-08-21 02:09:19 +0900

    netlify 배포 수정9

--- functions/handleQuestion.js
+++ functions/handleQuestion.js
@@ -3,14 +3,24 @@ import fetch from "node-fetch";
 export async function handler(event) {
   const url = "https://api.openai.com/v1/chat/completions";
   const api = process.env.GPT_KEY; // 환경 변수에서 Secret Key 읽어오기
+  const requestBody = JSON.parse(event.body);
 
+  // `gpt.js`에서 전달되는 데이터 형식에 맞게 헤더와 본문을 구성합니다.
   const options = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${api}`,
     },
-    body: JSON.stringify(JSON.parse(event.body)),
+    body: JSON.stringify({
+      model: requestBody.model,
+      messages: requestBody.messages,
+      temperature: requestBody.temperature,
+      max_tokens: requestBody.max_tokens,
+      top_p: requestBody.top_p,
+      frequency_penalty: requestBody.frequency_penalty,
+      presence_penalty: requestBody.presence_penalty,
+    }),
   };
 
   try {
