c8377df2858d2ff8fe57a74331f67b9d97248a70
novellus1013 <novellus1013@gmail.com>
2023-08-21 02:02:41 +0900

    netlify 배포 수정8

--- functions/handleQuestion.js
+++ functions/handleQuestion.js
@@ -1,6 +1,8 @@
-import fetch from "node-fetch";
+let fetch;
 
 export async function handler(event) {
+  if (!fetch) fetch = await import("node-fetch").then((mod) => mod.default);
+
   const url = "https://api.openai.com/v1/chat/completions";
   const api = process.env.GPT_KEY; // 환경 변수에서 Secret Key 읽어오기
 
